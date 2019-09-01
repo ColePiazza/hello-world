751afd31a0ec97ff2712520c4f870e86488f2b03
ColePiazza <54772848+ColePiazza@users.noreply.github.com>
2019-09-01 10:59:54 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+def greet(name: str):
+    print("Hello " + name)
+
+def test_greet():
+    name = 'Cole'
+    greet(name)
+
+if __name__ == '__main__':
+    name = input("Cole Piazza: ")
+    greet(Cole)
